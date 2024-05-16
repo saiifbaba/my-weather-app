ebd88d85f827d259b33267f1dea9d5e98520f46d
kaifbaba <kaifbaba111@gmail.com>
2024-05-16 16:00:02 +0530

    created a weather app with cities

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'fa9f2ab37amsh0fef40b88e33a77p1a703djsnb4407835e515',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+ export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+ export const WEATHER_API_KEY ="815aa7ce22b5c5120ed9b5de3300f1b0";
